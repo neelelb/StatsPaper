e03f10d6569b0c4c3c39a531601f00296b9bbbb4
Neele <neele.lb@gmail.com>
2022-03-12 18:25:22 +0100

    import update

--- import.R
+++ import.R
@@ -23,41 +23,30 @@ exp_data$diff = ((exp_data$B-exp_data$A)/exp_data$A) %>% round(digits = 3)
 
 # create age group variable
 exp_data$agegroup=0
-for (i in 1:length(exp_data$ID)){
-  if (exp_data$ID[i] <= 362){
-    exp_data$agegroup[i] = "younger adults"}
-  else if (exp_data$ID[i] > 362){
-    exp_data$agegroup[i] = "older adults"} 
-}
+exp_data$agegroup[exp_data$ID <= 362] = "younger adults"
+exp_data$agegroup[exp_data$ID > 362] = "older adults"
 
 # create conflict variable
 exp_data$conflict = 0
-for (i in 1:length(exp_data$diff)){
-  if (exp_data$diff[i] <= 0.01 | exp_data$diff[i] == 0.50){
-    exp_data$conflict[i] = "low conflict"}
-  else {
-    exp_data$conflict[i] = "high conflict"} 
-}
+exp_data$conflict[exp_data$diff <= 0.01 | exp_data$diff == 0.50] = "low conflict"
+exp_data$conflict[exp_data$diff > 0.01 & exp_data$diff < 0.50] = "high conflict"
 
-# create option variable coding "immediate" or "delayed" condition
-exp_data$option = 0
-for (i in 1:length(exp_data$DA)){
-  if (exp_data$DA[i] == 7){
-    exp_data$option[i] = "immediate"}
-  else if (exp_data$DA[i] == 14){
-    exp_data$option[i] = "delayed"} 
-}
+# create condition variable coding "immediate" or "delayed" condition
+exp_data$condition = 0
+exp_data$condition[exp_data$DA == 7]  = "immediate"
+exp_data$condition[exp_data$DA == 14]  = "delayed"
 
 # create delay variable translating delay of B into weeks
 exp_data$delay = 0
-for (i in 1:length(exp_data$DB)){
-  if (exp_data$DB[i] == 14){
-    exp_data$delay[i] = "two weeks"}
-  else if (exp_data$DB[i] == 28){
-    exp_data$delay[i] = "four weeks"} 
-  else if (exp_data$DB[i] == 42){
-    exp_data$delay[i] = "six weeks"} 
-}
+exp_data$delay[exp_data$DB == 14]  = "two weeks"
+exp_data$delay[exp_data$DB == 28]  = "four weeks"
+exp_data$delay[exp_data$DB == 42]  = "six weeks"
+
+# create choice of participants as character variable
+exp_data$choice = 0
+exp_data$choice[exp_data$R == 1] = "late" 
+exp_data$choice[exp_data$R == 0] = "early" 
+
 
 
 #--------- DATA IMPORT COVARIATES ---------#
@@ -78,7 +67,7 @@ data = left_join(exp_data, cov_all, by = c("agegroup", "ID"))
 
 # write final data to text file to make import easy in script 2
 # this way, variables are created once and stored for use in another session
-write.table(data, 'data/data_delay_conditioning.txt',
+write.table(data, 'data/data_all.txt',
             sep = ",", dec = ".", row.names = FALSE, col.names = TRUE)
 
 
